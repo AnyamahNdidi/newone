f1cc0d714a9a20a0cda5946b196ca3ad1bf29694
AnyamahNdidi <theo4felix@gmail.com>
2022-04-14 08:44:07 +0100

    this is it

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require("./config/db")
+const PORT = process.env.PORT || 2023
 const express = require("express")
 const myRouter = require("./Router/router")
 const dotenv = require("dotenv")
 
 const app = express()
-const PORT = process.env.PORT || 2023
+
 
 dotenv.config()
-app.get("/",(req, res)=>{
-  res.status(200).json({messgae :"api is ready"})
-})
+// app.get("/",(req, res)=>{
+//   res.status(200).json({messgae :"api is ready"})
+// })
 
 app.use("/api", myRouter )
 
