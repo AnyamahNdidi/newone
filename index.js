5adf39f04669684592cf806a52e4e4db338006e7
AnyamahNdidi <theo4felix@gmail.com>
2022-04-14 08:38:51 +0100

    api put

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 require("./config/db")
 const express = require("express")
-
+const myRouter = require("./Router/router")
+const dotenv = require("dotenv")
 
 const app = express()
 const PORT = process.env.PORT || 2023
 
-
+dotenv.config()
 app.get("/",(req, res)=>{
   res.status(200).json({messgae :"api is ready"})
 })
+
+app.use("/api", myRouter )
 
 app.listen(PORT, ()=>{
   console.log( `port is runnig on ${PORT}`)
